62911cb73d27cedd7da349f601cf405e519d46f5
ashutoshpandey2802 <ashutosh.p.2802@gmail.com>
2024-06-08 12:30:21 +0530

    Fixed url Issue

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,14 +4,9 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.views import generic
 
-
-
 # Create your views here.
-
-
 def home(request):
     return render(request,'dashboard/home.html')
-
 
 def notes(request):
     if request.method=="POST":
@@ -22,17 +17,16 @@ def notes(request):
         
         messages.success(request,f'notes addded from {request.user.username} succussfully')
     
-    
-    form=NotesForms()
+    else:
+         form=NotesForms()
+         
     notes=Notes.objects.filter(user=request.user)
     context={'notes':notes,"form": form}
     return render(request,'dashboard/notes.html',context)
 
-
 def delete_note(request,pk=None):
     Notes.objects.get(id=pk).delete()
     return redirect("notes")
-
 
 def NotesDetailView ( DetailView):
     model=Notes
